a6eb4c3b6f5730a3fc44c3ee4450093ce8c76e40
Jackbear8868 <leo1314520528868@gmail.com>
2025-03-13 20:33:35 +0800

    veryfast

--- student_agent.py
+++ student_agent.py
@@ -7,7 +7,7 @@ import random
 from train import QNetwork  # Ensure QNetwork is in the same directory
 
 # Load the trained model once at the start
-MODEL_PATH = "dqn_taxi-510.pth"
+MODEL_PATH = "dqn_taxi.pth"
 STATE_SIZE = 16  # Ensure this matches your environment
 ACTION_SIZE = 6
 
